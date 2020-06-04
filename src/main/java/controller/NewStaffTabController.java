c1432318e59c86b183ab0595a9cc5778a9dbf7cf
Dan <Daniel.bloodworth18@my.northampton.ac.uk>
2020-06-04 16:57:18 +0100

    Data updating for staff edit tab.

--- src/main/java/controller/NewStaffTabController.java
+++ src/main/java/controller/NewStaffTabController.java
@@ -21,57 +21,61 @@ public class NewStaffTabController {
 
     public void createStaff() throws SQLException {
         if (!verifyFilledInputFields()) {
-            String dbURL = "jdbc:mysql://localhost:3306/rmsdb";
-            String username = "root";
-            String password = "root";
-            String query = "INSERT INTO staff(status, dormancy_reason, first_name, middle_name, surname, gender, address_number, address_house_name, address_street, address_town, address_county, " +
-                    "address_country, zip_code, telephone, email_address, emergency_contact_phone, emergency_contact_email, specialist_subject, resume, additional_notes, medical_history, medical_allergies, medical_religious, image) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            Connection myConnection = DriverManager.getConnection(dbURL, username, password);
-            PreparedStatement preparedStatement = myConnection.prepareStatement(query);
-            preparedStatement.setString(1, statusCB.getValue().toString());
-            if (!statusCB.getValue().toString().equals("Dormant")) {
-                preparedStatement.setString(2, "Inactive");
+            if (idTF.getText().equalsIgnoreCase("automatically generated")) {
+                String dbURL = "jdbc:mysql://localhost:3306/rmsdb";
+                String username = "root";
+                String password = "root";
+                String query = "INSERT INTO staff(status, dormancy_reason, first_name, middle_name, surname, gender, address_number, address_house_name, address_street, address_town, address_county, " +
+                        "address_country, zip_code, telephone, email_address, emergency_contact_phone, emergency_contact_email, specialist_subject, resume, additional_notes, medical_history, medical_allergies, medical_religious, image) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                Connection myConnection = DriverManager.getConnection(dbURL, username, password);
+                PreparedStatement preparedStatement = myConnection.prepareStatement(query);
+                preparedStatement.setString(1, statusCB.getValue().toString());
+                if (!statusCB.getValue().toString().equals("Dormant")) {
+                    preparedStatement.setString(2, "Inactive");
+                } else {
+                    preparedStatement.setString(2, dormCB.getValue().toString());
+                }
+                preparedStatement.setString(3, firstNameTF.getText());
+                preparedStatement.setString(4, middleNameTF.getText());
+                preparedStatement.setString(5, surnameTF.getText());
+                preparedStatement.setString(6, genderCB.getValue().toString());
+                preparedStatement.setString(7, addNumberTF.getText());
+                preparedStatement.setString(8, houseNTF.getText());
+                preparedStatement.setString(9, houseSTF.getText());
+                preparedStatement.setString(10, houseTTF.getText());
+                preparedStatement.setString(11, countyTF.getText());
+                preparedStatement.setString(12, countryTF.getText());
+                preparedStatement.setString(13, zipTF.getText());
+                preparedStatement.setString(14, phoneTF.getText());
+                preparedStatement.setString(15, emailTF.getText());
+                preparedStatement.setString(16, emergPTF.getText());
+                preparedStatement.setString(17, emergETF.getText());
+                preparedStatement.setString(18, specialismCB.getValue().toString());
+                preparedStatement.setString(19, "resume");
+                preparedStatement.setString(20, addNoteTA.getText());
+                preparedStatement.setString(21, medicalHTA.getText());
+                preparedStatement.setString(22, allergyTA.getText());
+                preparedStatement.setString(23, religiousTA.getText());
+                preparedStatement.setString(24, "image");
+                preparedStatement.executeUpdate();
+                Statement fetchStaff = myConnection.createStatement();
+                ResultSet getStaff = fetchStaff.executeQuery("SELECT * FROM staff WHERE first_name='" + firstNameTF.getText() + "'AND surname='" + surnameTF.getText() + "'");
+                String newUser = "INSERT INTO users(username, password, user_level, course, staff_id) VALUES (?,?,?,?,?)";
+                PreparedStatement newUserStatement = myConnection.prepareStatement(newUser);
+                newUserStatement.setString(1, firstNameTF.getText() + "" + surnameTF.getText());
+                newUserStatement.setString(2, firstNameTF.getText() + "" + surnameTF.getText());
+                newUserStatement.setInt(3, 4);
+                newUserStatement.setString(4, courseCB.getValue().toString());
+                while (getStaff.next()) {
+                    newUserStatement.setString(5, String.valueOf(getStaff.getInt("staff_id")));
+                }
+                newUserStatement.execute();
+                System.out.println("Staff Created!");
+                Stage stage = (Stage) saveBtn.getScene().getWindow();
+                stage.close();
             } else {
-                preparedStatement.setString(2, dormCB.getValue().toString());
+                updateStaff();
             }
-            preparedStatement.setString(3, firstNameTF.getText());
-            preparedStatement.setString(4, middleNameTF.getText());
-            preparedStatement.setString(5, surnameTF.getText());
-            preparedStatement.setString(6, genderCB.getValue().toString());
-            preparedStatement.setString(7, addNumberTF.getText());
-            preparedStatement.setString(8, houseNTF.getText());
-            preparedStatement.setString(9, houseSTF.getText());
-            preparedStatement.setString(10, houseTTF.getText());
-            preparedStatement.setString(11, countyTF.getText());
-            preparedStatement.setString(12, countryTF.getText());
-            preparedStatement.setString(13, zipTF.getText());
-            preparedStatement.setString(14, phoneTF.getText());
-            preparedStatement.setString(15, emailTF.getText());
-            preparedStatement.setString(16, emergPTF.getText());
-            preparedStatement.setString(17, emergETF.getText());
-            preparedStatement.setString(18, specialismCB.getValue().toString());
-            preparedStatement.setString(19, "resume");
-            preparedStatement.setString(20, addNoteTA.getText());
-            preparedStatement.setString(21, medicalHTA.getText());
-            preparedStatement.setString(22, allergyTA.getText());
-            preparedStatement.setString(23, religiousTA.getText());
-            preparedStatement.setString(24, "image");
-            preparedStatement.executeUpdate();
-            Statement fetchStaff = myConnection.createStatement();
-            ResultSet getStaff = fetchStaff.executeQuery("SELECT * FROM staff WHERE first_name='" + firstNameTF.getText() + "'AND surname='" + surnameTF.getText() + "'");
-            String newUser = "INSERT INTO users(username, password, user_level, course, staff_id) VALUES (?,?,?,?,?)";
-            PreparedStatement newUserStatement = myConnection.prepareStatement(newUser);
-            newUserStatement.setString(1, firstNameTF.getText() + "" + surnameTF.getText());
-            newUserStatement.setString(2, firstNameTF.getText() + "" + surnameTF.getText());
-            newUserStatement.setInt(3, 4);
-            newUserStatement.setString(4, courseCB.getValue().toString());
-            while (getStaff.next()) {
-                newUserStatement.setString(5, String.valueOf(getStaff.getInt("staff_id")));
-            }
-            newUserStatement.execute();
-            System.out.println("Student Created!");
-            Stage stage = (Stage) saveBtn.getScene().getWindow();
-            stage.close();
         }
     }
 
@@ -192,7 +196,24 @@ public class NewStaffTabController {
             medicalHTA.setText(getStaff.getString("medical_history"));
             courseCB.setValue("Computing");
         }
+    }
 
-
+    public void updateStaff() throws SQLException {
+        String dbURL = "jdbc:mysql://localhost:3306/rmsdb";
+        String username = "root";
+        String password = "root";
+        String query = ("UPDATE staff SET status='" + statusCB.getValue().toString() + "', dormancy_reason='" + dormCB.getValue().toString() + "', first_name='" + firstNameTF.getText() + "', middle_name='" +
+                middleNameTF.getText() + "', surname='" + surnameTF.getText() + "', gender='" + genderCB.getValue().toString() + "', address_number='" + addNumberTF.getText() + "', address_house_name='" +
+                houseNTF.getText() + "', address_street='" + houseSTF.getText() + "', address_town='" + houseTTF.getText() + "', address_county='" + countyTF.getText() +
+                "', address_country='" + countryTF.getText() + "', zip_code='" + zipTF.getText() + "', telephone='" + phoneTF.getText() + "', email_address='" + emailTF.getText() +
+                "', emergency_contact_phone='" + emergPTF.getText() + "', emergency_contact_email='" + emergETF.getText() + "', specialist_subject='" + specialismCB.getValue().toString() +
+                "', resume='" + "resume" + "', additional_notes='" + addNoteTA.getText() + "', medical_history='" + medicalHTA.getText() + "', medical_allergies='" +
+                allergyTA.getText() + "', medical_religious='" + religiousTA.getText() + "', image='" + "image" + "' WHERE staff_id='" + idTF.getText() + "'");
+        Connection myCon = DriverManager.getConnection(dbURL, username, password);
+        PreparedStatement preparedStatement = myCon.prepareStatement(query);
+        preparedStatement.execute();
+        System.out.println("Staff Updated!");
+        Stage stage = (Stage) saveBtn.getScene().getWindow();
+        stage.close();
     }
 }
