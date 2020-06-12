4a9c4386b0dc6ddfed45d16c5320c9947c3d82c8
Dan <Daniel.bloodworth18@my.northampton.ac.uk>
2020-06-12 18:53:38 +0100

    Fix timetable saving.

--- src/main/java/view/TimetableWindow.java
+++ src/main/java/view/TimetableWindow.java
@@ -4,10 +4,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import jfxtras.icalendarfx.VCalendar;
 import jfxtras.scene.control.agenda.icalendar.ICalendarAgenda;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.Period;
 
@@ -19,22 +26,32 @@ public class TimetableWindow {
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 1366, 768);
         Stage stage = new Stage();
-        setupComponents(root, agenda);
+        setupComponents(root, newCal);
         root.setCenter(agenda);
         stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
 
-    private void setupComponents(BorderPane root, ICalendarAgenda agenda) {
+    private void setupComponents(BorderPane root, VCalendar calendar) {
         Button saveButton = new Button("Save Timetable");
 
         HBox buttonBox = new HBox(saveButton);
         root.setTop(buttonBox);
 
         saveButton.setOnAction((e)-> {
-            LocalDateTime newLocalDateTime = agenda.getDisplayedLocalDateTime().plus(Period.ofWeeks(1));
-            agenda.setDisplayedLocalDateTime(newLocalDateTime);
+            FileChooser fileSaver = new FileChooser();
+            fileSaver.setTitle("Save cause for concern form");
+            File destination = fileSaver.showSaveDialog(root.getScene().getWindow());
+            try {
+                if (destination != null) {
+                    PrintWriter timetableFile = new PrintWriter(destination + ".txt");
+                    timetableFile.println(calendar.toString());
+                    System.out.println(calendar.toString());
+                }
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
         });
     }
 }
