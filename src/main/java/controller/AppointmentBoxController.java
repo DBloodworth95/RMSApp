a4a1f616e070bf3347f9cb42e5f820e0ab906a01
Dan <Daniel.bloodworth18@my.northampton.ac.uk>
2020-06-17 14:55:51 +0100

    Implemented PAT Form window.

--- src/main/java/controller/AppointmentBoxController.java
+++ src/main/java/controller/AppointmentBoxController.java
@@ -2,19 +2,33 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import view.PATWindow;
+
+import java.io.IOException;
 
 public class AppointmentBoxController {
     @FXML
     private Text studentT, idT, dateT;
     @FXML
     private HBox hBox;
+    @FXML
+    private Button uploadBtn;
 
     public void setValues(String student, String id, String date) {
         hBox.setMargin(hBox, new Insets(10,0,10,0));
         studentT.setText(student);
         idT.setText(id);
         dateT.setText(date);
+    }
+
+    public void openPATWindow() throws IOException {
+        new PATWindow();
+    }
+
+    public void setUploadBtnToUploaded() {
+        uploadBtn.setText("PAT Uploaded");
     }
 }
---
+++ src/main/java/view/PATWindow.java
@@ -0,0 +1,26 @@
+package view;
+
+import controller.BookingController;
+import controller.PATFormController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PATWindow {
+
+    public PATWindow() throws IOException {
+        FXMLLoader createCourseLoader = new FXMLLoader();
+        createCourseLoader.setLocation(getClass().getResource("/FXMLview/PATForm.fxml"));
+        Scene scene = new Scene(createCourseLoader.load(), 1448, 580);
+        Stage stage = new Stage();
+        stage.setTitle("PAT Form");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setMaximized(false);
+        stage.show();
+        Object temp = createCourseLoader.getController();
+        PATFormController controller = (PATFormController) temp;
+    }
+}
