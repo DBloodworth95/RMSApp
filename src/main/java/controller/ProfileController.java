f1d7fa00b810077bf13250a6203e3f18a0cdaa3d
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-06-24 14:35:00 +0100

    Added single entry-point.

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,7 +1,7 @@
 package controller;
 
 
-import javafx.embed.swing.SwingFXUtils;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,33 +1,7 @@
 package main;
 
-import controller.HomePageController;
-import controller.LoginController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-
-public class Main extends Application {
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLview/Login.fxml"));
-        Parent loginPage = loader.load();
-        Scene scene = new Scene(loginPage);
-//        scene.getStylesheets().add("main.css");
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Woodlands RMS");
-        Object temp = loader.getController();
-        LoginController controller = (LoginController) temp;
-        controller.initial();
-        primaryStage.setMaximized(true);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        UI.main(args);
     }
 }
